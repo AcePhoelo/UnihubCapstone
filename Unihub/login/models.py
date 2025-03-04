2eb75a3a72189ece0beef7cbb2a768b72744b064
Hans Hartowidjojo <hanshw33@gmail.com>
2025-03-05 02:39:03 +0800

    Connected Database and Added .gitignore
    I connected the database I created with MariaDB server and tried making a table for login, I've also added user samples via the ipython shell and added .gitignore to ignore cache files when committing

--- Unihub/login/models.py
+++ Unihub/login/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models (database) here.
+class Account(models.Model):
+    student_id = models.CharField(
+        max_length=8,
+        unique=True,
+        validators=[RegexValidator(regex=r'^\d{8}$', message='Student ID must be 8 digits')]
+
+    )
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=128)  # Use a larger max_length for hashed passwords
+
+    # This is a string representation of the accounts
+    def __str__(self):
+        return(f"{self.student_id}")
+    
--- Unihub/login/tests.py
+++ Unihub/login/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+'''
+Test case 1: 
+Student ID: 21449723
+Name: Hans
+Password: 12345678
+
+
+Test case 2:
+Student ID: 29723144
+Name: Snah
+Password: 87654321
+'''
