02bfaee197d743fa056e4665a0bf6e904c78b01f
Sonder105 <adeliawanti1509@gmail.com>
2025-05-06 15:44:47 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,18 +31,18 @@ DEBUG = True
 
 # Use the same host for the frontend for simplicity (we choose localhost here)
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
     "http://54.255.76.59:8000",
+    "http://localhost:3000",
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Frontend origin
     "http://54.255.76.59:8000",
+    "http://localhost:3000",  # Frontend origin
 ]
 ALLOWED_HOSTS = [
-    "localhost",  # Frontend
     "54.255.76.59", #AWS EC2
     "54.255.76.59:8000",
+    "localhost",  # Frontend
 ]
 
 
