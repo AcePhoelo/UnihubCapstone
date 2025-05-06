8acaf95b0357d42bbb21d5670f931a3543fc5a05
Sonder105 <adeliawanti1509@gmail.com>
2025-05-06 15:36:24 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR.parent / "my-app" / "public"]
+STATICFILES_DIRS = [BASE_DIR.parent / "my-app" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
