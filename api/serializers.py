3c3901a2d580060fbde07929dfe42939c94d241e
Sonder105 <adeliawanti1509@gmail.com>
2025-05-07 13:03:51 +0800

    Delete api directory

--- api/serializers.py
+++
@@ -1,4 +0,0 @@
-# serializers.py
-
-# No custom serializer is needed now. You can use the default TokenObtainPairSerializer.
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import login_view, set_csrf_cookie
-
-urlpatterns = [
-    path("login/", login_view, name="login"),
-    path("csrf/", set_csrf_cookie, name="set_csrf_cookie"),
-]
