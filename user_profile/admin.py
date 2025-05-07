181b3526631097c66e46d23041edf2d9f4619a53
Sonder105 <adeliawanti1509@gmail.com>
2025-05-07 13:13:13 +0800

    Delete user_profile directory

--- user_profile/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Student
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'email', 'studentid', 'profile_picture')  # Display profile picture in admin
--- user_profile/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProfileConfig(AppConfig):  # Renamed to match the app name
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_profile'
-
-    def ready(self):
-        import user_profile.signals  # noqa: F401 - Import is required to register signals
