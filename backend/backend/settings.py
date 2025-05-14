6615fc8b4a4f4472f0e74ad9901ebaff45645d3f
Hans Hartowidjojo <hanshw33@gmail.com>
2025-05-14 16:45:52 +0800

    domain:8000

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,8 @@ ALLOWED_HOSTS = [
     "localhost",  # Frontend local
     "127.0.0.1",  # Backend (change to 54.169.81.75:8000)
     "54.169.81.75",
+    "curtinunihubplus.com:8000",
+    "www.curtinunihubplus.com",
 ]
 
 
